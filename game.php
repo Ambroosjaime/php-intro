4ac9820bf80c4851e16822ca6dd8b88fb7c4205a
Ambroos Jaime <46563759+Ambroosjaime@users.noreply.github.com>
2019-05-12 21:46:35 +0200

    Add files via upload

---
+++ game.php
@@ -0,0 +1,21 @@
+<pre>
+    <?php
+    
+    class blackjack{
+
+public $card =[];
+public $score = array();
+public function hit(){
+
+    function stand(){
+
+    }
+
+    function surrender(){
+
+    }
+}
+};
+
+    ?>
+<pre>
